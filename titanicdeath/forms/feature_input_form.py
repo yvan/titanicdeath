d6008c80c185005ccbbce5faab0db27684c740fc
yvan <yvanscher@gmail.com>
2017-04-23 22:03:30 -0400

    updates

--- titanicdeath/forms/feature_input_form.py
+++ titanicdeath/forms/feature_input_form.py
@@ -4,7 +4,7 @@ from wtforms import validators
 
 class FeatureInput(FlaskForm):
     sex = RadioField('sex', choices=[('man','a man'),('woman','a woman')], validators=[validators.Required()])
-    pclass = RadioField('passenger_class', choices=[('1','first class'),('2','second class'), ('3','third class')], validators=[validators.Required()], render_kw={"placeholder": "what class is your ticket in?"})
+    pclass = RadioField('passenger_class', choices=[('1','first class'),('2','business'), ('3','economy')], validators=[validators.Required()], render_kw={"placeholder": "what class is your ticket in?"})
     married = RadioField('married', choices=[('married','married'),('unmarried','unmarried')], validators=[validators.Required()]) 
     age = StringField('age', validators=[validators.Required()], render_kw={"placeholder": "how old are you?"})
     siblings = StringField('siblings', validators=[validators.Required()], render_kw={"placeholder": "how many siblings do you have?"})
