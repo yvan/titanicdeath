e7722f9b058e7d246f71585033d6b6d34441a0c3
yvan <yvanscher@gmail.com>
2017-04-25 20:31:26 -0400

    fixed validation error using datarequired

--- titanicdeath/forms/feature_input_form.py
+++ titanicdeath/forms/feature_input_form.py
@@ -6,8 +6,8 @@ class FeatureInput(FlaskForm):
     sex = RadioField('sex', choices=[('man','a man'),('woman','a woman')], validators=[validators.Required()])
     pclass = RadioField('passenger_class', choices=[('1','first class'),('2','business'), ('3','economy')], validators=[validators.Required()], render_kw={"placeholder": "what class is your ticket in?"})
     married = RadioField('married', choices=[('married','married'),('unmarried','unmarried')], validators=[validators.Required()]) 
-    age = IntegerField('age', validators=[validators.Required()], render_kw={"placeholder": "how old are you?"})
-    siblings = IntegerField('siblings', validators=[validators.Required()], render_kw={"placeholder": "how many siblings do you have?"})
+    age = IntegerField('age', validators=[validators.InputRequired()], render_kw={"placeholder": "how old are you?"})
+    siblings = IntegerField('siblings', validators=[validators.InputRequired()], render_kw={"placeholder": "how many siblings do you have?"})
 
 '''
 author @yvan
